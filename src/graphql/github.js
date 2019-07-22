b5f2d98e86bf68766cb618e2fa371a5e1fdbf917
hurkan.akbiyik <hurkan.akbiyik@bestseller.com>
2019-07-22 10:35:42 +0200

    Init graphql call for github

---
+++ src/graphql/github.js
@@ -0,0 +1,74 @@
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import fetch from "isomorphic-fetch";
+import gql from "graphql-tag";
+
+// overwrite accept */* header as graphiql will return html if application/json is not specified
+// specifying headers for createHttpLink will just add, not overwrite
+const overwriteHeadersFetch = (uri, options) => {
+    options.headers = {
+        Accept: "application/json",
+        Authorization: "Token a15011436fbfdb0f3128f5038ebe9a17bbb91580"
+    };
+    return fetch(uri, options);
+};
+
+export const client = new ApolloClient({
+    link: createHttpLink({
+        fetch: overwriteHeadersFetch,
+        uri: "https://api.github.com/graphql"
+    }),
+    cache: new InMemoryCache()
+});
+
+
+const QUERY = gql`
+    query repoWithIssues(
+        $owner:String!
+        $name:String!
+        $field: IssueOrderField!
+        $direction: OrderDirection!
+        $after: String
+    ) {
+        repository(owner: $owner, name: $name) {
+            issues(
+                first: 10
+                orderBy: { field: $field, direction: $direction }
+                after: $after
+            ) {
+                nodes {
+                    url
+                }
+                totalCount
+                pageInfo {
+                    endCursor
+                    startCursor
+                }
+            }
+        }
+    }
+`;
+
+/**
+ *
+ * @param owner - repo owner
+ * @param name - repo name
+ * @param field
+ * @param direction
+ * @param after
+ * @returns {Promise<ApolloQueryResult<any> | never>}
+ */
+export const fetchRepositoryIssues = ({owner, name, field, direction, after}) =>
+    client
+        .query({
+            variables: {
+                owner,
+                name,
+                field,
+                direction,
+                after
+            },
+            query: QUERY
+        })
+        .then(res => res.data);
